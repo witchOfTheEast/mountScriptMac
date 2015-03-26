8469e3af24ed0f83db055d27288acef106513dd2
Randall Dunn <justThisGuyRandall@gmail.com>
2015-03-26 10:48:52 -0700

    Added mount of share from MAC server. NOTE: Requires afp, an AUTH option, and FQDN. Additionally, modified mount_TYPE to mount -t TYPE to reflect best practices.

--- mountShare.sh
+++ mountShare.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-<<<<<<< HEAD
 # To create a user folder in /Volumes/ and mount //<NETWORKSHARE>/Users/<LOGNAMEFOLDER>
+# mount MAC based shares require type afp, option AUTH, and FQDN
 
 mount_point_base="/Volumes/${LOGNAME}"
-share_point_root="//uifs1/Users/"
+mount_point_one="${mount_point_base}"
+mount_point_two="${mount_point_base}_two"
 
-if [[ ! -d ${mount_point_base} ]]; then
-    mkdir ${mount_point_base}
+share_point_one_root="//uifs1/Users/"
+share_point_two_root="uimacmini.ui.local1/"
+
+auth=";AUTH=Client%20KRB%20v2@"
+
+if [[ ! -d ${mount_point_one} ]]; then
+    mkdir ${mount_point_one}
 fi
 
-/sbin/mount_smbfs ${share_point_root}${LOGNAME} ${mount_point_base}
+/sbin/mount -t smbfs ${share_point_one_root}${LOGNAME} ${mount_point_one}
+
+
+if [[ ! -d ${mount_point_two} ]]; then
+    mkdir ${mount_point_two}
+fi
+
+/sbin/mount -t afp "afp://${auth}${share_point_two_root}${LOGNAME}" ${mount_point_two}
