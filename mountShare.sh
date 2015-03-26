12498e3732caaa403bab4b3387ec419fdf1c19fa
System Administrator <root@UIMacBookPro.local>
2015-03-26 09:06:34 -0700

    Replaced hardcode paths with variables

---
+++ mountShare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# To create a user folder in /Volumes/ and mount //<NETWORKSHARE>/Users/<LOGNAMEFOLDER>
+
+mount_point_base="/Volumes/${LOGNAME}"
+share_point_root="//uifs1/Users/"
+
+if [[ ! -d ${mount_point_base} ]]; then
+    mkdir ${mount_point_base}
+fi
+
+/sbin/mount_smbfs ${share_point_root}${LOGNAME} ${mount_point_base}
+
